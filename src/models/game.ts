7443cddf01bab1d2835978747ee426997749b8ba
Tobias Fernkorn <fernsikos@gmail.com>
2022-11-07 12:23:27 +0700

    bug

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ["Klaus","Klaus","Klaus","Klaus","Klaus","Klaus","Klaus","Klaus","Klaus",];
+    public players: string[] = [];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
