f0ad2905683eb69417850f789217f6f93dfd65ba
Tobias Fernkorn <fernsikos@gmail.com>
2022-11-07 11:07:14 +0700

    fixes

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-dialog-add-player',
@@ -9,13 +11,15 @@ export class DialogAddPlayerComponent implements OnInit {
 
   name: string = "";
 
-  constructor() { }
+  constructor(    public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { 
+    
+  }
 
   ngOnInit(): void {
   }
 
   onNoClick() {
-    
+    this.dialogRef.close();
   }
 
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -42,8 +42,8 @@ export class GameComponent implements OnInit {
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
     });
-
     dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length > 0) 
       this.game.players.push(name);
     });
   }
